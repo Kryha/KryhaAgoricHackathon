a80d0398f1872156e51b0b2bf7162ef07065e5d4
Terry <terry@kryha.io>
2020-05-18 16:59:45 +0200

    Remove old store init from store

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -38,9 +38,3 @@ export function reducer (state, action) {
       return state
   }
 }
-
-// export function StoreProvider ({children}) {
-//   const [state, dispatch] = React.useReducer(reducer, initialState)
-//   const value = { state, dispatch }
-//   return <Store.Provider value={value}>{children}</Store.Provider>
-// }
